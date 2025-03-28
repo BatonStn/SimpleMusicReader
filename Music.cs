b2abe5c35c463f22dbcfeeccec746b5f5bddf15b
Baton_ <baptiste.s@hotmail.fr>
2025-03-28 19:56:21 +0100

    Bug Fix: Could not open Folder containing songs with no album picture

--- Music.cs
+++ Music.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
+using System.Windows;
 
 namespace SimpleMusicReader;
 
@@ -29,27 +30,52 @@ public class Music
     public Music(string path, int songNumber)
     {
         Path = path;
+
         TagLib.File file = TagLib.File.Create(path);
 
         Title = file.Tag.Title;
+
         Artists = "";
         foreach (string artist in file.Tag.AlbumArtists)
         {
             Artists += artist + ", ";
         }
-        Artists = Artists.Remove(Artists.Length - 2);
+        if (Artists.Length > 0) Artists = Artists.Remove(Artists.Length - 2);
+
         Album = file.Tag.Album;
 
-        Cover = LoadImage(file.Tag.Pictures[0].Data.Data);
+        if (file.Tag.Pictures.Length > 0) Cover = LoadImage(file.Tag.Pictures[0].Data.Data);
+        else
+        {
+            string folder = Directory.GetParent(path).FullName;
+            foreach (string fileFromFolder in Directory.GetFiles(folder))
+            {
+                string extension = System.IO.Path.GetExtension(fileFromFolder);
+                if (extension == ".jpg" || extension == ".png" || extension == ".jpeg")
+                {
+                    Cover = LoadImage(File.ReadAllBytes(fileFromFolder));
+                    break;
+                }
+            }
+            Cover ??= LoadImage(null);
+        }
+
         SongNumber = songNumber;
     }
 
     private static BitmapImage LoadImage(byte[] imageData)
     {
-        if (imageData == null || imageData.Length == 0) return null;
         var image = new BitmapImage();
-        using (var mem = new MemoryStream(imageData))
+        if (imageData == null || imageData.Length == 0)
         {
+            image.BeginInit();
+            image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+            image.CacheOption = BitmapCacheOption.OnLoad; image.UriSource = new Uri(@"questionmark.jpg", UriKind.Relative);
+            image.EndInit();
+        }
+        else
+        {
+            using var mem = new MemoryStream(imageData);
             mem.Position = 0;
             image.BeginInit();
             image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
